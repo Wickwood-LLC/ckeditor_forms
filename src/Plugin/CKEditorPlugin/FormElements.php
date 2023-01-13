8b8e8c3330989450c856b6aa82083c11aab9a555
albaparticle <palagpatski@gmail.com>
2023-01-14 00:21:48 +0800

    initial commit

---
+++ src/Plugin/CKEditorPlugin/FormElements.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\ckeditor_forms\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "forms" plugin.
+ *
+ * @CKEditorPlugin(
+ *   id = "ckeditor_forms",
+ *   label = @Translation("CKEditor Form Elements"),
+ *   module = "ckeditor_forms"
+ * )
+ */
+class FormElements extends CKEditorPluginBase
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFile()
+    {
+        return drupal_get_path('module', 'ckedior_forms') . '/js/forms.js';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getButtons()
+    {
+        $libraryUrl = $this->getLibraryUrl();
+
+        return [
+            'Form Elements' => array(
+                'label' => $this->t('Form Elements'),
+                'image' => $libraryUrl . '/icons/form.png',
+            ),
+        ];
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function isEnabled(Editor $editor)
+    {
+        return TRUE;
+    }
+
+    /**
+     * Get the CKEditor Form Elements library URL.
+     */
+    protected function getLibraryUrl()
+    {
+
+        $originUrl = \Drupal::request()->getSchemeAndHttpHost() . \Drupal::request()->getBaseUrl();
+
+        $librarayPath = DRUPAL_ROOT . '/libraries/forms';
+        $librarayUrl = $originUrl . '/libraries/forms';
+
+        // Is the library found in the root libraries path.
+        $libraryFound = file_exists($librarayPath . '/plugin.js');
+
+        // If library is not found, then look in the current profile libraries path.
+        if (!$libraryFound) {
+            if ($installProfile = \Drupal::installProfile()) {
+                $profilePath = drupal_get_path('profile', $installProfile);
+                $profilePath .= '/libraries/forms';
+
+                // Is the library found in the current profile libraries path.
+                if (file_exists(DRUPAL_ROOT . '/' . $profilePath . '/plugin.js')) {
+                    $libraryFound = TRUE;
+                    $librarayUrl = $originUrl . '/' . $profilePath;
+                }
+            }
+        }
+
+        if ($libraryFound) {
+            return $librarayUrl;
+        } else {
+            return $originUrl . '/libraries/forms';
+        }
+    }
+}
