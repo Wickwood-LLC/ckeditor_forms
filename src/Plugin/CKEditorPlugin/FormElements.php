da05545ebaab1c054d2ae21e542c7827882be9f9
albaparticle <palagpatski@gmail.com>
2023-01-14 01:27:22 +0800

    Trying a different way to find the libraries

--- src/Plugin/CKEditorPlugin/FormElements.php
+++ src/Plugin/CKEditorPlugin/FormElements.php
@@ -22,7 +22,20 @@ class FormElements extends CKEditorPluginBase
      */
     public function getFile()
     {
-        return $this->getLibraryPath() . '/plugin.js';
+        $plugin_paths = [
+            'libraries/forms/plugin.js',
+            'libraries/ckeditor/plugins/forms/plugin.js',
+        ];
+
+        $plugin = FALSE;
+        foreach ($plugin_paths as $plugin_path) {
+            if (file_exists(DRUPAL_ROOT . '/' . $plugin_path)) {
+                $plugin = $plugin_path;
+                break;
+            }
+        }
+
+        return $plugin;
     }
 
     /**
@@ -30,12 +43,23 @@ class FormElements extends CKEditorPluginBase
      */
     public function getButtons()
     {
-        $libraryUrl = $this->getLibraryUrl();
+        $library_paths = [
+            'libraries/forms',
+            'libraries/ckeditor/plugins/forms',
+        ];
+
+        $library = FALSE;
+        foreach ($library_paths as $library_path) {
+            if ($library_path) {
+                $library = $library_path;
+                break;
+            }
+        }
 
         return [
             'Form Elements' => array(
                 'label' => $this->t('Form Elements'),
-                'image' => $libraryUrl . '/icons/form.png',
+                'image' => $library . '/icons/form.png',
             ),
         ];
     }
@@ -53,75 +77,5 @@ class FormElements extends CKEditorPluginBase
     public function getConfig(Editor $editor)
     {
         return [];
-    }
-
-    /**
-     * Get the CKEditor Form Elements library path.
-     */
-    protected function getLibraryPath()
-    {
-        // Following the logic in Drupal 8.9.x and Drupal 9.x
-        // ----------------------------------------------------------------------
-        // Issue #3096648: Add support for third party libraries in site specific
-        // and install profile specific libraries folders
-        // https://www.drupal.org/project/drupal/issues/3096648
-        //
-        // https://git.drupalcode.org/project/drupal/commit/1edf15f
-        // -----------------------------------------------------------------------
-        // Search sites/<domain>/*.
-        $directories[] = \Drupal::service('site.path') . "/libraries/";
-
-        // Always search the root 'libraries' directory.
-        $directories[] = 'libraries/';
-
-        // Installation profiles can place libraries into a 'libraries' directory.
-        if ($installProfile = \Drupal::installProfile()) {
-            $profile_path = drupal_get_path('profile', $installProfile);
-            $directories[] = "$profile_path/libraries/";
-        }
-
-        foreach ($directories as $dir) {
-            if (file_exists(DRUPAL_ROOT . '/' . $dir . 'forms/plugin.js')) {
-                return $dir . 'forms';
-            }
-        }
-
-        return 'libraries/forms';
-    }
-
-
-    /**
-     * Get the CKEditor Form Elements library URL.
-     */
-    protected function getLibraryUrl()
-    {
-
-        $originUrl = \Drupal::request()->getSchemeAndHttpHost() . \Drupal::request()->getBaseUrl();
-
-        $libraryPath = DRUPAL_ROOT . '/libraries/forms';
-        $libraryUrl = $originUrl . '/libraries/forms';
-
-        // Is the library found in the root libraries path.
-        $libraryFound = file_exists($libraryPath . '/plugin.js');
-
-        // If library is not found, then look in the current profile libraries path.
-        if (!$libraryFound) {
-            if ($installProfile = \Drupal::installProfile()) {
-                $profilePath = drupal_get_path('profile', $installProfile);
-                $profilePath .= '/libraries/forms';
-
-                // Is the library found in the current profile libraries path.
-                if (file_exists(DRUPAL_ROOT . '/' . $profilePath . '/plugin.js')) {
-                    $libraryFound = TRUE;
-                    $libraryUrl = $originUrl . '/' . $profilePath;
-                }
-            }
-        }
-
-        if ($libraryFound) {
-            return $libraryUrl;
-        } else {
-            return $originUrl . '/libraries/forms';
-        }
     }
 }
